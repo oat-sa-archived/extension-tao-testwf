11ba9a9f02b6ece5ebe83ca9a6ba97cd58a5fdb6
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-07 13:59:10 +0000

    added report support
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoWfTest/trunk@18947 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/class.WfTestCompiler.php
+++ models/classes/class.WfTestCompiler.php
@@ -28,54 +28,86 @@
  */
 class taoWfTest_models_classes_WfTestCompiler extends taoTests_models_classes_TestCompiler
 {
+    /**
+     * (non-PHPdoc)
+     * @see tao_models_classes_Compiler::compile()
+     */
     public function compile() {
         
+        $report = new common_report_Report(common_report_Report::TYPE_SUCCESS,__('Published test "%s"', $this->getResource()->getLabel()));
         common_ext_ExtensionsManager::singleton()->getExtensionById('taoWfTest'); // loads the extension
         
         $test = $this->getResource();
         common_Logger::i('Compiling test ' . $test->getLabel().' items');
         $process = $test->getUniquePropertyValue(new core_kernel_classes_Property(TEST_TESTCONTENT_PROP));
         $processCloner = new wfAuthoring_models_classes_ProcessCloner();
-        $processClone = $processCloner->cloneProcess($process);
+        try {
+            $processClone = $processCloner->cloneProcess($process);
+            $report->add(new common_report_Report(common_report_Report::TYPE_SUCCESS,__('Cloned the process %s', $process->getLabel())));
+        } catch (common_Exception $e) {
+            $report->add(new common_report_Report(common_report_Report::TYPE_ERROR,__('Failed to clone the process')));
+            $report->setType(common_report_Report::TYPE_ERROR);
+        }
         
-        $this->process($processClone);
+        $itemsServiceReport = $this->process($processClone);
+        foreach ($itemsServiceReport as $subReport) {
+            $report->add($subReport);
+        }
         
-        $serviceCall = new tao_models_classes_service_ServiceCall(new core_kernel_classes_Resource(INSTANCE_SERVICE_PROCESSRUNNER));
-        $param = new tao_models_classes_service_ConstantParameter(
-            new core_kernel_classes_Resource(INSTANCE_FORMALPARAM_PROCESSDEFINITION),
-            $processClone->getUri()
-        );
-        $serviceCall->addInParameter($param);
-        return $serviceCall;
+        if ($itemsServiceReport->getType() == common_report_Report::TYPE_SUCCESS) {
+            $serviceCall = new tao_models_classes_service_ServiceCall(new core_kernel_classes_Resource(INSTANCE_SERVICE_PROCESSRUNNER));
+            $param = new tao_models_classes_service_ConstantParameter(new core_kernel_classes_Resource(INSTANCE_FORMALPARAM_PROCESSDEFINITION), $processClone->getUri());
+            $serviceCall->addInParameter($param);
+            
+            $report->setData($serviceCall);
+        } else {
+            $report->setType(common_report_Report::TYPE_ERROR);
+        }
+
+        if ($report->getType() != common_report_Report::TYPE_SUCCESS) {
+            $report->setMessage(__('Failed to publish test "%s".' , $this->getResource()->getLabel()));
+        }
+        
+        return $report;
     }
-    
+
+    /**
+     * Walk through the cloned process and replace the item runner placeholders
+     *
+     * @param core_kernel_classes_Resource $processDefinition            
+     */
     protected function process(core_kernel_classes_Resource $processDefinition)
     {
+        $report = new common_report_Report(common_report_Report::TYPE_SUCCESS);
         $activities = wfEngine_models_classes_ProcessDefinitionService::singleton()->getAllActivities($processDefinition);
         foreach ($activities as $activity) {
             $services = wfEngine_models_classes_ActivityService::singleton()->getInteractiveServices($activity);
             foreach ($services as $service) {
                 $serviceDefinition = $service->getUniquePropertyValue(new core_kernel_classes_Property(PROPERTY_CALLOFSERVICES_SERVICEDEFINITION));
                 if ($serviceDefinition->getUri() == INSTANCE_ITEMCONTAINER_SERVICE) {
-                    $newService = $this->getItemRunnerService($service);
-                    // remove old service
-                    wfEngine_models_classes_InteractiveServiceService::singleton()->deleteInteractiveService($service);
-                    $activity->removePropertyValue(new core_kernel_classes_Property(PROPERTY_ACTIVITIES_INTERACTIVESERVICES), $service);
-                    // add new service
-                    $activity->setPropertyValue(new core_kernel_classes_Property(PROPERTY_ACTIVITIES_INTERACTIVESERVICES), $newService);
+                    $item = taoWfTest_models_classes_WfTestService::singleton()->getItemByService($service);
+                    if (is_null($item)) {
+                        $report->add($this->fail(__('No valid item found for service "%s"', $service->getLabel())));
+                    } else {
+                        $itemReport = $this->subCompile($item);
+                        if ($itemReport->getType() == common_report_Report::TYPE_SUCCESS) {
+                            $serviceCall = $itemReport->getData();
+                            $storedServiceCall = $serviceCall->toOntology();
+                            
+                            // remove old service
+                            wfEngine_models_classes_InteractiveServiceService::singleton()->deleteInteractiveService($service);
+                            $activity->removePropertyValue(new core_kernel_classes_Property(PROPERTY_ACTIVITIES_INTERACTIVESERVICES), $service);
+                            // add new service
+                            $activity->setPropertyValue(new core_kernel_classes_Property(PROPERTY_ACTIVITIES_INTERACTIVESERVICES), $storedServiceCall);
+                        }
+                        $report->add($itemReport);
+                        if ($itemReport->getType() != common_report_Report::TYPE_SUCCESS) {
+                            $report->setType($itemReport->getType());
+                        }
+                    }
                 }
-                // todo if process called, flatten
             }
         }
-    }
-    
-    protected function getItemRunnerService(core_kernel_classes_Resource $service)
-    {
-        $item = taoWfTest_models_classes_WfTestService::singleton()->getItemByService($service);
-        if (is_null($item)) {
-            throw new taoWfTest_models_classes_MalformedServiceCall($service, 'No valid item found for service '.$service->getUri());
-        }
-        $callService = $this->subCompile($item);
-        return $callService->toOntology();
+        return $report;
     }
 }
